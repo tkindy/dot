1366cf259499133343826c18a48c7dc4431624a2
Tyler Kindy <tylerkindy@gmail.com>
2018-09-18 14:53:38 -0400

    lastpass-cli

--- setup.fish
+++ setup.fish
@@ -3,7 +3,8 @@
 set CONFIG_DIR "$HOME/.config"
 
 # Install the essentials
-sudo apt-get install -y curl fonts-firacode > /dev/null
+echo "Installing essential packages..."
+sudo apt-get install -y curl fonts-firacode lastpass-cli > /dev/null
 
 # Install Oh My Fish
 if string match $OMF_CONFIG ""
@@ -20,4 +21,12 @@ else
   echo "Setting Fish theme to agnoster..."
   omf theme agnoster > /dev/null
 end
+
+
+# TODO more config
+
+
+# Clean up
+echo "Cleaning up auto installed packages..."
+sudo apt-get autoremove > /dev/null
 
