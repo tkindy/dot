debde119940dd0e53f7f87d414af907ab06398a1
Tyler Kindy <tylerkindy@gmail.com>
2018-09-18 10:13:41 -0400

    Install Oh My Fish, set theme

--- setup.fish
+++ setup.fish
@@ -1,4 +1,18 @@
 #!/usr/bin/fish
 
-echo "Run other setup"
+# Install the essentials
+sudo apt-get install -y curl fonts-firacode > /dev/null
+
+# Install Oh My Fish
+if string match $OMF_CONFIG ""
+  echo "Installing Oh My Fish..."
+  curl -L https://get.oh-my.fish | fish > /dev/null
+else
+  echo "Oh My Fish is already installed"
+end
+
+# Set agnoster theme
+# TODO: make incremental
+echo "Setting Fish theme to agnoster..."
+omf theme agnoster > /dev/null
 
--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function check-exit() {
+  if [ $? -ne 0 ]; then
+    exit
+  fi
+}
+
 # Check that the script is being run as a normal user
 if [ "$EUID" -eq 0 ]; then
   echo "Please run as a normal user, not as root."
@@ -10,7 +16,9 @@ fi
 echo
 echo "Upgrading packages..."
 sudo apt-get update > /dev/null
-sudo apt-get upgrade -y /dev/null
+check-exit
+sudo apt-get upgrade -y > /dev/null
+check-exit
 
 # Install Fish
 echo
@@ -19,6 +27,7 @@ fishPath=$(which fish)
 if [ -z "$fishPath" ]; then
   echo "Installing Fish..."
   sudo apt-get install -y fish > /dev/null
+  check-exit
 else
   echo "Fish is already installed"
 fi
@@ -29,6 +38,7 @@ echo
 if [ "$SHELL" != "/usr/bin/fish" ]; then
   echo "Enter your password to change your login shell to Fish"
   chsh -s /usr/bin/fish
+  check-exit
 else
   echo "Fish is already your login shell"
 fi
