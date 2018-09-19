1881f49a5545dd2efd32b501e24882adcf2acb54
Tyler Kindy <tylerkindy@gmail.com>
2018-09-19 18:47:37 -0400

    Use stow, use global instead of universal

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,17 @@
+# Clear greeting
+set -g fish_greeting ""
+
+# Git Abbreviations
+add-alias g   "git"
+add-alias ga  "git add"
+add-alias gb  "git branch"
+add-alias gx  "git checkout"
+add-alias gxm "git checkout master"
+add-alias gp  "git pull"
+add-alias gu  "git push"
+add-alias gd  "git diff"
+add-alias gh  "git stash"
+add-alias ghl "git stash list"
+add-alias ghp "git stash pop"
+add-alias ghd "git stash drop"
+
--- fish/.config/fish/functions/add-alias.fish
+++ fish/.config/fish/functions/add-alias.fish
@@ -1,5 +1,5 @@
 # Defined in /tmp/fish.SMdbs9/add-alias.fish @ line 2
 function add-alias
 	set abbr "$argv[1]=$argv[2]"
-	set -U fish_user_abbreviations $fish_user_abbreviations $argv[1]
+	set -g fish_user_abbreviations $fish_user_abbreviations $abbr
 end
