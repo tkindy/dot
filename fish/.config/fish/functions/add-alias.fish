e357ebb4c87b930f1238ec467a9588178a53754f
Tyler Kindy <tylerkindy@gmail.com>
2018-09-19 21:16:55 -0400

    Add description to add-alias

--- fish/.config/fish/functions/add-alias.fish
+++ fish/.config/fish/functions/add-alias.fish
@@ -1,5 +1,4 @@
-# Defined in /tmp/fish.SMdbs9/add-alias.fish @ line 2
-function add-alias
+function add-alias -d "Create a Fish abbreviation"
 	set abbr "$argv[1]=$argv[2]"
 	set -g fish_user_abbreviations $fish_user_abbreviations $abbr
 end
