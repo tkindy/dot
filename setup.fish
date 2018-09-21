9f07a175a8aebafab6b5012dd07a9545b43e913e
Tyler Kindy <tylerkindy@gmail.com>
2018-09-21 16:10:39 -0400

    Add vim-plug installation

--- setup.fish
+++ setup.fish
@@ -40,6 +40,7 @@ else
   echo "Installing vim-plug..."
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  vim +PlugInstall +q +q
 end
 
 
