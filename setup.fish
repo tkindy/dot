018753313c9cfa9fd8e440f04840136797b10ab5
Tyler Kindy <tylerkindy@gmail.com>
2019-01-18 10:43:40 -0500

    Also get Java 8

--- setup.fish
+++ setup.fish
@@ -44,7 +44,7 @@ sudo apt-get update
 sudo apt-get install -y vim-gtk fonts-firacode lastpass-cli tree python tmux \
                         nordvpn xclip make make-doc ruby-full build-essential \
                         signal-desktop ocaml-nox opam openjdk-11-jdk-headless \
-                        xloadimage
+                        openjdk-8-jdk-headless xloadimage
 
 sudo snap install spotify vlc
 sudo snap install --classic slack 
