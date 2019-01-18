72141290d1a1521518349d4f435d9186d7835939
Tyler Kindy <tylerkindy@gmail.com>
2019-01-18 10:41:51 -0500

    Add xloadimage

--- setup.fish
+++ setup.fish
@@ -43,7 +43,8 @@ echo "Installing general packages..."
 sudo apt-get update
 sudo apt-get install -y vim-gtk fonts-firacode lastpass-cli tree python tmux \
                         nordvpn xclip make make-doc ruby-full build-essential \
-                        signal-desktop ocaml-nox opam openjdk-11-jdk-headless
+                        signal-desktop ocaml-nox opam openjdk-11-jdk-headless \
+                        xloadimage
 
 sudo snap install spotify vlc
 sudo snap install --classic slack 
