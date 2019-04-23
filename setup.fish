30d290677b97547df9b9b7830de4ef6595bcd206
Tyler Kindy <tylerkindy@gmail.com>
2019-04-23 15:54:25 -0400

    Install Transmission

--- setup.fish
+++ setup.fish
@@ -44,7 +44,7 @@ sudo apt-get update
 sudo apt-get install -y vim-gtk fonts-firacode lastpass-cli tree python tmux \
                         nordvpn xclip make make-doc ruby-full build-essential \
                         signal-desktop ocaml-nox opam openjdk-12-jdk xloadimage \
-                        feh
+                        feh transmission
 
 sudo snap install spotify vlc gimp
 sudo snap install --classic intellij-idea-community
