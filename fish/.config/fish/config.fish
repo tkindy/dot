088e9a61b28ff20e4122d0afef64072c3afd6f75
Tyler Kindy <tylerkindy@gmail.com>
2022-03-10 17:55:27 -0500

    Change some stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,8 +6,17 @@ set -x VISUAL vim
 set -x EDITOR $VISUAL
 
 set -x BREW_HOME "/opt/homebrew"
+set -x CARGO_HOME "$HOME/.cargo"
+set -x GEM_HOME "/opt/homebrew/lib/ruby/gems/3.0.0"
+set -x RUBY_HOME "/opt/homebrew/opt/ruby"
+set -x NIX_HOME "$HOME/.nix-profile"
+set -x SDKROOT (xcrun --show-sdk-path)
 
 set -x PATH "$BREW_HOME/bin" \
+            "$CARGO_HOME/bin" \
+            "$GEM_HOME/bin" \
+            "$RUBY_HOME/bin" \
+            "$NIX_HOME/bin" \
             $PATH
 
 # Use Vim keybindings
@@ -16,3 +25,4 @@ fish_vi_key_bindings
 set -x GPG_TTY (tty)
 
 source ~/.config/fish/abbrs.fish
+fish_add_path /opt/homebrew/opt/openjdk@11/bin
