c2740dcaa9709a2e2b83d0c98b8b6ece6e7242da
Tyler Kindy <tylerkindy@gmail.com>
2022-04-13 19:46:22 -0400

    More changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,27 +4,20 @@ set -g fish_greeting ""
 # Set environment variables
 set -x VISUAL vim
 set -x EDITOR $VISUAL
+set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/openjdk-17.jdk/Contents/Home"
 set -x GEM_HOME "$HOME/.gems"
 set -x NVM_HOME "$HOME/.nvm"
-set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/"
-set -x JDK_HOME $JAVA_HOME
 set -x CARGO_HOME "$HOME/.cargo"
 set -x ANACONDA_HOME "/usr/local/anaconda3"
 
-set -x PATH $PATH \
+set -x PATH "$JAVA_HOME/bin" \
             "$GEM_HOME/bin" \
             "$CARGO_HOME/bin" \
-            "$ANACONDA_HOME/bin"
+            "$ANACONDA_HOME/bin" \
+            $PATH
 
 # Use Vim keybindings
 fish_vi_key_bindings
-
-# OPAM configuration
-source /home/tyler/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
-
-if status --is-login; and test (tty) = "/dev/tty1"
-  exec startx
-end
 
 # Added by nex: https://git.hubteam.com/HubSpot/nex
 source ~/.hubspot/config.fish
