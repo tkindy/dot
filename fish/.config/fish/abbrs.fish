65ff9ea09726b6d97af2af4949e774684d7c7989
Tyler Kindy <tylerkindy@gmail.com>
2023-10-26 10:05:42 -0400

    Add some shell aliases

--- fish/.config/fish/abbrs.fish
+++ fish/.config/fish/abbrs.fish
@@ -1,14 +1,16 @@
 if status --is-interactive
+  # git
   abbr --add --global g   git
   abbr --add --global ga  'git add'
   abbr --add --global gb  'git branch'
   abbr --add --global gc  'git commit'
   abbr --add --global gd  'git diff'
-  abbr --add --global gh  'git stash'
-  abbr --add --global ghd 'git stash drop'
-  abbr --add --global ghl 'git stash list'
-  abbr --add --global ghp 'git stash pop'
-  abbr --add --global ghs 'git stash show -p'
+  abbr --add --global gds 'git diff --staged'
+  abbr --add --global gj  'git stash'
+  abbr --add --global gjd 'git stash drop'
+  abbr --add --global gjl 'git stash list'
+  abbr --add --global gjp 'git stash pop'
+  abbr --add --global gjs 'git stash show -p'
   abbr --add --global gl  'git log'
   abbr --add --global glg 'git log --oneline --graph --decorate --branches'
   abbr --add --global gp  'git pull'
@@ -20,4 +22,20 @@ if status --is-interactive
   abbr --add --global gu  'git push'
   abbr --add --global gx  'git checkout'
   abbr --add --global gxm 'git checkout master'
+  abbr --add --global ge  'git rebase'
+  abbr --add --global gem 'git rebase master'
+
+  # git-machete
+  abbr --add --global gm  'git machete'
+  abbr --add --global gms 'git machete status'
+  abbr --add --global gmd 'git machete diff'
+  abbr --add --global gmc 'git machete discover'
+  abbr --add --global gmt 'git machete traverse --fetch --whole'
+
+  # GitHub CLI
+  abbr --add --global ghpr 'gh pr create --web'
+
+  # Maven
+  abbr --add --global mcv 'mvn clean verify'
+  abbr --add --global mst 'mvn clean verify -DskipTests -DskipValidation'
 end
