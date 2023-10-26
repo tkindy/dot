8a4a66bc9a69607ac98d798220b3b545bbc6ff74
Tyler Kindy <tylerkindy@gmail.com>
2023-10-26 10:06:20 -0400

    Use JAVA17_HOME variable set by automation

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,20 +1,21 @@
+# Added by nex: https://git.hubteam.com/HubSpot/nex
+source ~/.hubspot/config.fish
+
 # Clear greeting
 set -g fish_greeting ""
 
 # Set environment variables
 set -x VISUAL vim
 set -x EDITOR $VISUAL
-set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/openjdk-17.jdk/Contents/Home"
+set -x JAVA_HOME $JAVA17_HOME
 
-set -x PATH "$JAVA_HOME/bin" \
-            "$HOME/bin" \
+set -x PATH "$HOME/bin" \
+            "$HOME/.yarn/bin" \
+            "$HOME/.rd/bin" \
             $PATH
 
 # Use Vim keybindings
 fish_vi_key_bindings
-
-# Added by nex: https://git.hubteam.com/HubSpot/nex
-source ~/.hubspot/config.fish
 
 set -x GPG_TTY (tty)
 
