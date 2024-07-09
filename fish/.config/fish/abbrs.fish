cd977aa9dee89517fa3556ef3329c520d57b5211
Tyler Kindy <tylerkindy@gmail.com>
2024-07-09 17:29:39 -0400

    Simplify Maven skip tests abbr

--- fish/.config/fish/abbrs.fish
+++ fish/.config/fish/abbrs.fish
@@ -47,6 +47,6 @@ if status --is-interactive
 
   # Maven
   abbr --add --global mcv  'mvn clean verify'
-  abbr --add --global mst  'mvn clean verify -DskipTests -DskipValidation'
+  abbr --add --global mst  'mvn clean test-compile -DskipValidation'
   abbr --add --global mdcv 'mvnd clean verify'
 end
