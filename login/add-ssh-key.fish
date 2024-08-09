a55dd94fe72227ef0f0665664b5b155e90e7da8b
Tyler Kindy <tylerkindy@gmail.com>
2024-08-09 14:11:51 -0400

    Inline script

--- login/add-ssh-key.fish
+++
@@ -1,9 +0,0 @@
-#!/opt/homebrew/bin/fish
-
-function add
-  echo "[$(date)] Adding SSH key"
-  /usr/bin/ssh-add --apple-use-keychain ~/.ssh/id_rsa
-  and echo "[$(date)] Added SSH key"
-end
-
-add &> ~/temp/add-ssh-key.log
